1a0adfc5a56b196664f1ef38a0f1c5ebae77495a
Denddy <denddyrizky@gmail.com>
2020-12-25 15:32:35 +0100

    Made the api services

--- app/src/main/java/com/example/madlevel6task2/MovieApiService.kt
+++ app/src/main/java/com/example/madlevel6task2/MovieApiService.kt
@@ -1,4 +1,12 @@
 package com.example.madlevel6task2
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val API_KEY = "cdcf101a73bdc4081aa17d52d3ef799e"
+
 interface MovieApiService {
+
+    @GET("discover/movie?api_key=$API_KEY&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+    suspend fun getMovies(@Query("year") year: Int): MovieList
 }
